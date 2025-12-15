4bc1a03bfedc740bc93122e878709651960ad098
Vy Thanh <thanhvy050500@gmail.com>
2025-12-15 07:38:50 +0700

    Add simple Node.js ackend

--- backend-node/login.js
+++ backend-node/login.js
@@ -1,1 +1,18 @@
-Code login. . .
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend Node.js is running 🚀");
+});
+
+app.get("/login", (req, res) => {
+  res.json({
+    success: true,
+    message: "Login API working"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
